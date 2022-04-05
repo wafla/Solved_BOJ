6b1bbf0be7f4add5c382a9fc832c474a8b82369c
최연우 <50083524+wafla@users.noreply.github.com>
2022-04-05 11:43:45 +0900

    [Platinum I] Title: Binary Game 2, Time: 232 ms, Memory: 98416 KB -BaekjoonHub

---
+++ 백준/Platinum/24901. Binary Game 2/Binary Game 2.cc
@@ -0,0 +1,63 @@
+//; # \u000a import java.util.*;public class Main{public static void main(String[] args){Scanner sc = new Scanner(System.in);int n = sc.nextInt();if(n==0)System.out.print("0");else if(n==1)System.out.print("01");else if(n==2)System.out.print("0110");else if(n==3) System.out.print("011011");else if(n==4)System.out.print("011011100");else if(n==5)System.out.print("011011100101");else if(n==6)System.out.print("011011100101110");else if(n==7)System.out.print("011011100101110111"); else if(n==8) System.out.print("0110111001011101111000");else if(n==9)System.out.print("01101110010111011110001001");else if(n==10)System.out.print("011011100101110111100010011010");}} /*
+#if true /*
+n = gets().to_i
+if n == 0
+    puts "0"
+elsif n == 1
+    puts "01"
+elsif n == 2
+    puts "0110"
+elsif n == 3
+    puts "011011"
+elsif n == 4
+    puts "011011100"
+elsif n == 5
+    puts "011011100101"
+elsif n == 6
+    puts "011011100101110"
+elsif n == 7
+    puts "011011100101110111"
+elsif n == 8
+    puts "0110111001011101111000"
+elsif n == 9
+    puts "01101110010111011110001001"
+elsif n == 10
+    puts "011011100101110111100010011010"
+end
+=begin
+=begin
+/* */ // \u000a /*
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n==0)
+        printf("0");
+    else if(n==1)
+        printf("01");
+    else if(n==2)
+        printf("0110");
+    else if(n==3)
+        printf("011011");
+    else if(n==4)
+        printf("011011100");
+    else if(n==5)
+        printf("011011100101");
+    else if(n==6)
+        printf("011011100101110");
+    else if(n==7)
+        printf("011011100101110111");
+    else if(n==8)
+        printf("0110111001011101111000");
+    else if(n==9)
+        printf("01101110010111011110001001");
+    else if(n==10)
+        printf("011011100101110111100010011010");
+}
+// */
+// */ let n = Int(readLine()!)!;if n==0{print("0")}else if n==1{print("01")}else if n==2{print("0110")}else if n==3{print("011011")}else if n==4{print("011011100")}else if n==5{print("011011100101")}else if n==6{print("011011100101110")}else if n==7{print("011011100101110111")}else if n==8{print("0110111001011101111000")}else if n==9{print("01101110010111011110001001")}else if n==10{print("011011100101110111100010011010")} // \u000a /*
+#endif
+/*
+=end
+# */
