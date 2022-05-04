6dd184b72273a1a0dcf50d601ed38dc5614ff66a
최연우 <50083524+wafla@users.noreply.github.com>
2022-05-04 11:04:06 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+  printf("         ,r'\"7\n");
+  printf("r`-_   ,'  ,/\n");
+  printf(" \\. \". L_r'\n");
+  printf("   `~\\/\n");
+  printf("      |\n");
+  printf("      |\n");
+}
