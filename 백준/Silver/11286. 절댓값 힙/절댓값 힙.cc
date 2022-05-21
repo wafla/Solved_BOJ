6b5a02efff745252648ef9c8cc5cfda3497fe086
최연우 <50083524+wafla@users.noreply.github.com>
2022-05-21 17:40:28 +0900

    [Silver I] Title: 절댓값 힙, Time: 20 ms, Memory: 3684 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <map>
+#include <stack>
+#include <memory.h>
+#include <deque>
+#include <set>
+#define X first
+#define Y second
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(NULL);
+	cin.tie(NULL), cout.tie(NULL);
+	priority_queue <pair<long long,int>> h;
+	int n;
+	cin >> n;
+	while (n--)
+	{
+		int x;
+		cin >> x;
+		if (x == 0)
+		{
+			if (h.size() == 0)
+				cout << 0 << '\n';
+			else {
+				cout << h.top().X * h.top().Y << '\n';
+				h.pop();
+			}
+		}
+		else {
+			if (x > 0)
+				h.push({ -x, -1 });
+			else
+				h.push({ x, 1 });
+		}
+	}
+}
