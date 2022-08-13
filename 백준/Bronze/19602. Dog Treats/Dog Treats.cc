79b729530ed1bf62c91431736dc964e615c1bfe7
최연우 <50083524+wafla@users.noreply.github.com>
2022-08-13 17:36:45 +0900

    [Bronze IV] Title: Dog Treats, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19602. Dog Treats/Dog Treats.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cin >> a >> b >> c;
+    if(a*1 + b*2 + c*3 >= 10)
+        cout << "happy";
+    else
+        cout << "sad";
+}
