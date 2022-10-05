5f9151d9294caafa158dbace365a7afd6c4b5b79
최연우 <50083524+wafla@users.noreply.github.com>
2022-10-05 19:37:42 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 4 ms, Memory: 2236 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
@@ -15,50 +15,49 @@
 #define MOD 1000000007
 #define ll long long
 using namespace std;
-int arr[126][126] = { 0 };
-int v[126][126] = { 0 };
+typedef pair<int, pair<int, int>> T;
 int dx[4] = { 1,-1,0,0 }, dy[4] = { 0,0,1,-1 };
+bool v[126][126] = { 0 };
+int arr[126][126] = { 0 };
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL), cout.tie(NULL);
-	int cnt = 1;
-	while (1)
-	{
-		int n;
-		cin >> n;
-		if (n == 0)
-			break;
-		memset(v, 0, sizeof(v));
-		memset(arr, 0, sizeof(arr));
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < n; j++)
-			{
-				cin >> arr[i][j];
-				v[i][j] = INF;
-			}
-		}
-		queue <pair<int, int>> Q;
-		Q.push({ 0,0 });
-		v[0][0] = arr[0][0];
-		while (!Q.empty())
-		{
-			auto cur = Q.front();
-			Q.pop();
-			for (int i = 0; i < 4; i++)
-			{
-				int nx = cur.X + dx[i];
-				int ny = cur.Y + dy[i];
-				if (nx < 0 || nx >= n || ny < 0 || ny >= n)
-					continue;
-				if (arr[nx][ny] + v[cur.X][cur.Y] < v[nx][ny])
-				{
-					v[nx][ny] = arr[nx][ny] + v[cur.X][cur.Y];
-					Q.push({ nx,ny });
-				}
-			}
-		}
-		cout << "Problem " << cnt++ << ": " << v[n - 1][n - 1] << '\n';
-	}
+    ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    int cnt = 1;
+    while (1)
+    {
+        memset(v, 0, sizeof(v));
+        memset(arr, 0, sizeof(arr));
+        int n;
+        cin >> n;
+        if (n == 0)
+            break;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                cin >> arr[i][j];
+        priority_queue<T, vector<T>, greater<T>> PQ;
+        PQ.push({ arr[0][0],{0,0} });
+        v[0][0] = 1;
+        while (!PQ.empty())
+        {
+            auto cur = PQ.top();
+            PQ.pop();
+            if (cur.Y.X == n - 1 && cur.Y.Y == n - 1)
+            {
+                cout << "Problem " << cnt++ << ": " << cur.X << '\n';
+                break;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                int nx = cur.Y.X + dx[i];
+                int ny = cur.Y.Y + dy[i];
+                if (nx < 0 || ny < 0 || nx >= n || ny >= n)
+                    continue;
+                if (v[nx][ny])
+                    continue;
+                v[nx][ny] = 1;
+                PQ.push({ cur.X + arr[nx][ny], {nx,ny} });
+            }
+        }
+    }
 }
