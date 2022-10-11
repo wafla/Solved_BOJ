bb01f2da4f9e6c1a3771c0ad4d5ec9870e8dda26
최연우 <50083524+wafla@users.noreply.github.com>
2022-10-11 14:12:59 +0900

    [Gold V] Title: A와 B 2, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/12919. A와 B 2/A와 B 2.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+#define INF 0x3f3f3f3f
+#define MOD 1000000007
+#define ll long long
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+	string s, str;
+	cin >> s >> str;
+	queue<string> Q;
+	Q.push(str);
+	while (!Q.empty())
+	{
+		auto cur = Q.front();
+		Q.pop();
+		if (cur.size() < s.size())
+			continue;
+		if (cur == s)
+		{
+			cout << 1;
+			return 0;
+		}
+		if (cur[cur.size() - 1] == 'A')
+		{
+			string tmp = cur;
+			tmp.pop_back();
+			Q.push(tmp);
+		}
+		if (cur[0] == 'B')
+		{
+			string tmp = cur;
+			reverse(tmp.begin(), tmp.end());
+			tmp.pop_back();
+			Q.push(tmp);
+		}
+	}
+	cout << 0;
+}
