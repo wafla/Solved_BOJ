81ec8b2d466c9ed7cfa718fa7d9eb39822df03e3
최연우 <50083524+wafla@users.noreply.github.com>
2022-12-16 12:40:35 +0900

    [Bronze I] Title: N번째 큰 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+#define INF 0x3f3f3f3f
+#define MOD 1000000007
+#define ll long long
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        vector<int> v(10);
+        for (int i = 0; i < 10; i++)
+            cin >> v[i];
+        sort(v.begin(), v.end());
+        cout << v[7] << '\n';
+    }
+}
