2ecd38bf91de08afa69cf67d284f6da90e2346c9
최연우 <50083524+wafla@users.noreply.github.com>
2022-12-23 19:46:33 +0900

    [Silver IV] Title: 전투의 신, Time: 180 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/26595. 전투의 신/전투의 신.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+#define INF 0x3f3f3f3f
+#define MOD 1000000007
+#define ll long long
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	ll n, a, x, b, y;
+	cin >> n >> a >> x >> b >> y;
+	ll cnt_a, cnt_b;
+	ll ans_a = 0, ans_b = 0, maxx = 0;
+	for (ll i = n; i >= 0; i--)
+	{
+		cnt_a = i / x;
+		cnt_b = (n - i) / y;
+		ll sum = a * cnt_a + b * cnt_b;
+		if (maxx < sum)
+		{
+			maxx = sum;
+			ans_a = cnt_a;
+			ans_b = cnt_b;
+		}
+	}
+	cout << ans_a << " " << ans_b << '\n';
+}
