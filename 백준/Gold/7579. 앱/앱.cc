3edd7087e68ed8b678d71f6df607a838fddac746
최연우 <50083524+wafla@users.noreply.github.com>
2023-01-09 21:41:12 +0900

    [Gold III] Title: 앱, Time: 8 ms, Memory: 9912 KB -BaekjoonHub

---
+++ 백준/Gold/7579. 앱/앱.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+#define INF 0x3f3f3f3f
+#define MOD 1000000007
+#define ll long long
+using namespace std;
+ll dp[101][10001] = { 0 };
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	ll n, m, sum = 0;
+	cin >> n >> m;
+	vector<pair<ll, ll>> v(n+1);
+	for (int i = 1; i <= n; i++)
+		cin >> v[i].X;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> v[i].Y;
+		sum += v[i].Y;
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 0; j <= sum; j++)
+		{
+			if (j - v[i].Y >= 0)
+				dp[i][j] = max(dp[i][j], dp[i - 1][j - v[i].Y] + v[i].X);
+			dp[i][j] = max(dp[i][j], dp[i - 1][j]);
+		}
+	}
+	for (int i = 0; i <= sum; i++)
+		if (dp[n][i] >= m)
+		{
+			cout << i << '\n';
+			break;
+		}
+}
+// 참고 : https://cocoon1787.tistory.com/319
