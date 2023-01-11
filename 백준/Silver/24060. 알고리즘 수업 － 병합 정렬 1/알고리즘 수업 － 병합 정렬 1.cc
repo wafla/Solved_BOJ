b4cfbc9a0132214ef8823b12fcc72fc311cb2865
최연우 <50083524+wafla@users.noreply.github.com>
2023-01-11 22:24:41 +0900

    [Silver IV] Title: 알고리즘 수업 - 병합 정렬 1, Time: 196 ms, Memory: 8196 KB -BaekjoonHub

---
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+#define INF 0x3f3f3f3f
+#define MOD 1000000007
+#define ll long long
+using namespace std;
+int arr[500001] = { 0 };
+int n, k, cnt = 0, ans = -1;
+void merge(int left, int mid, int right)
+{
+	int i = left, j = mid + 1;
+	vector<int> tmp;
+	while (i <= mid && j <= right)
+	{
+		if (arr[i] <= arr[j])
+			tmp.push_back(arr[i++]);
+		else
+			tmp.push_back(arr[j++]);
+	}
+	while (i <= mid)
+		tmp.push_back(arr[i++]);
+	while (j <= right)
+		tmp.push_back(arr[j++]);
+	int idx = left;
+	for (int i = 0; i < tmp.size(); i++)
+	{
+		cnt++;
+		if (cnt == k)
+			ans = tmp[i];
+		arr[left++] = tmp[i];
+	}
+}
+void merge_sort(int left, int right)
+{
+	if (left < right)
+	{
+		int mid = (left + right) / 2;
+		merge_sort(left, mid);
+		merge_sort(mid + 1, right);
+		merge(left, mid, right);
+	}
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	cin >> n >> k;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+	merge_sort(0, n - 1);
+	cout << ans << '\n';
+}
