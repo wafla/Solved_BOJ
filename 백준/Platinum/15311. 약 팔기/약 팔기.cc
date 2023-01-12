50bbe9edbc1f2ade5852cb4f390c33df7e06309e
최연우 <50083524+wafla@users.noreply.github.com>
2023-01-12 20:29:14 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "2000\n";
+    for(int i=0;i<1000;i++)
+        cout << 1 << " ";
+    for(int i=0;i<1000;i++)
+        cout << 1000 << " ";
+}
