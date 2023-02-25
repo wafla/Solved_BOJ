c341f4ed87e3d9690512f88eaf200d72f30ad0ef
최연우 <50083524+wafla@users.noreply.github.com>
2023-02-25 14:38:11 +0900

    [Silver V] Title: 이장님 초대, Time: 28 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Silver/9237. 이장님 초대/이장님 초대.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+#define INF 0x3f3f3f3f
+#define MOD 1000000007
+#define ll long long
+using namespace std;
+bool cmp(int x, int y)
+{
+	return x > y;
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++)
+		cin >> v[i];
+	sort(v.begin(), v.end(), cmp);
+	int sum = v[0];
+	for (int i = 0; i < n; i++)
+		v[i] += i + 1;
+	sort(v.begin(), v.end(), cmp);
+	cout << v[0] + 1 << '\n';
+}
