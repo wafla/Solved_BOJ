a7f34a52eb293f46194c928caad56ed45f9fbc59
최연우 <50083524+wafla@users.noreply.github.com>
2023-02-25 17:18:20 +0900

    [Platinum V] Title: 부분배열 고르기 2, Time: 40 ms, Memory: 21436 KB -BaekjoonHub

---
+++ 백준/Platinum/1989. 부분배열 고르기 2/부분배열 고르기 2.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+#define INF 0x3f3f3f3f
+#define MOD 1000000007
+#define ll long long
+using namespace std;
+int n;
+ll arr[100001] = { INF };
+vector<pair<ll,ll>> tree;
+pair<ll, ll> init(int node, int start, int end)
+{
+	if (start == end)
+		return tree[node] = { arr[start],start };
+	int mid = start + (end - start) / 2;
+	pair<ll, ll> left = init(node * 2, start, mid);
+	pair<ll, ll> right = init(node * 2 + 1, mid + 1, end);
+	return tree[node] = { left.X + right.X, (arr[left.Y] < arr[right.Y]) ? left.Y : right.Y };
+}
+pair<ll, ll> solve(int node, int st, int en, int left, int right)
+{
+	if (en < left || right < st)
+		return { 0,0 };
+	if (left <= st && en <= right)
+		return { tree[node].X, tree[node].Y };
+	int mid = st + (en - st) / 2;
+	pair<ll, ll> leftVal = solve(node * 2, st, mid, left, right);
+	pair<ll, ll> rightVal = solve(node * 2 + 1, mid + 1, en, left, right);
+	return { leftVal.X + rightVal.X, (arr[leftVal.Y] < arr[rightVal.Y]) ? leftVal.Y : rightVal.Y };
+}
+pair<ll, pair<ll,ll>> Div(int left, int right)
+{
+	if (left == right)
+		return { arr[left] * arr[left], {left, left} };
+	auto cur = solve(1, 1, n, left, right);
+	pair<ll, pair<ll, ll>> ans = { cur.X * arr[cur.Y],{left, right} };
+	if (cur.Y - 1 >= left)
+		ans = max(ans, Div(left, cur.Y - 1));
+	if (cur.Y + 1 <= right)
+		ans = max(ans, Div(cur.Y + 1, right));
+	return ans;
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		cin >> arr[i];
+	tree.resize(n * 4);
+	init(1, 1, n);
+	pair<ll, pair<ll, ll>> ans = Div(1, n);
+	cout << ans.X << '\n';
+	cout << ans.Y.X << " " << ans.Y.Y << '\n';
+}
