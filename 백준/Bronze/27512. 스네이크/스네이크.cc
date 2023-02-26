debac71b3beb8df074ea081849ca3ca473fdba3d
최연우 <50083524+wafla@users.noreply.github.com>
2023-02-27 01:11:57 +0900

    [Bronze V] Title: 스네이크, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27512. 스네이크/스네이크.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+#define INF 0x3f3f3f3f
+#define MOD 1000000007
+#define ll long long
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	int n, m;
+	cin >> n >> m;
+	if (n % 2 == 1 && m % 2 == 1)
+		cout << n * m - 1 << '\n';
+	else
+		cout << n * m << '\n';
+}
