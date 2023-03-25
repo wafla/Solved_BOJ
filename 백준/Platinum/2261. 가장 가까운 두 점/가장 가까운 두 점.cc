d73edc27b6372a68fef55771c98f9729b3e88329
최연우 <50083524+wafla@users.noreply.github.com>
2023-03-25 13:42:31 +0900

    [Platinum II] Title: 가장 가까운 두 점, Time: 728 ms, Memory: 3956 KB -BaekjoonHub

---
+++ 백준/Platinum/2261. 가장 가까운 두 점/가장 가까운 두 점.cc
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+#define INF 0x3f3f3f3f
+#define MOD 1000000007
+#define ll long long
+using namespace std;
+vector<pair<int, int>> v;
+int n;
+int dist(pair<int, int> a, pair<int, int> b)
+{
+	int x = (a.X - b.X) * (a.X - b.X);
+	int y = (a.Y - b.Y) * (a.Y - b.Y);
+	return x + y;
+}
+int solve(int l, int r)
+{
+	int size = r - l + 1;
+
+	if (size == 2)
+		return dist(v[l], v[r]);
+	else if (size == 3)
+		return min(dist(v[l], v[l + 1]), min(dist(v[l], v[r]), dist(v[l + 1], v[r])));
+
+	int mid = (l + r) / 2;
+	int ret = min(solve(l, mid), solve(mid + 1, r));
+
+	vector<pair<int, int>> p;
+	p.reserve(n); // 확보해놓은 용량, 공간 재할당이 필요 없음 == 시간 절약
+
+	int line = (v[mid].X + v[mid + 1].X) / 2;
+	for (int i = l; i <= r; i++)
+		if (line - v[i].X < ret)
+			p.push_back({v[i].Y, v[i].X}); // 다음번엔 Y축 기준으로 최소값 구함
+	sort(p.begin(), p.end());
+	
+	for (int i = 0; i < p.size(); i++)
+	{
+		for (int j = i + 1; j < p.size(); j++)
+		{
+			if ((p[i].X - p[j].X) * (p[i].X - p[j].X) >= ret)
+				break;
+			else
+				ret = min(ret, dist(p[i], p[j]));
+		}
+	}
+	return ret;
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int x, y;
+		cin >> x >> y;
+		v.push_back({ x,y });
+	}
+	sort(v.begin(), v.end());
+	cout << solve(0, n - 1) << '\n';
+}
+// 참고 : https://hackids.tistory.com/61
