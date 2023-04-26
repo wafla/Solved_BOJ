06aa14488dc67080564e9d5a811e2f46c1942213
최연우 <50083524+wafla@users.noreply.github.com>
2023-04-26 19:27:13 +0900

    [Bronze V] Title: 초코바, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+#define INF 0x3f3f3f3f
+#define MOD 1000000007
+#define ll long long
+#define pi pair<int,int>
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	int n, m;
+	cin >> n >> m;
+	if (n * 100 >= m)
+		cout << "Yes\n";
+	else
+		cout << "No\n";
+}
