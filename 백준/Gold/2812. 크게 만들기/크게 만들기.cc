b30fd052e77f8abbb55551b16db5632b194f950f
최연우 <50083524+wafla@users.noreply.github.com>
2023-05-03 17:19:06 +0900

    [Gold III] Title: 크게 만들기, Time: 12 ms, Memory: 3864 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+#define INF 0x3f3f3f3f
+#define MOD 1000000007
+#define ll long long
+#define pi pair<int, int>
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	int n, k, t, idx = 0;
+	cin >> n >> k;
+	t = k;
+	string s;
+	cin >> s;
+	deque<int> dq;
+	while (k && idx < s.size())
+	{
+		while (!dq.empty() && k)
+		{
+			if (dq.back() >= s[idx])
+				break;
+			else
+			{
+				dq.pop_back();
+				k--;
+			}
+		}
+		dq.push_back(s[idx++]);
+	}
+	while (idx < s.size())
+		dq.push_back(s[idx++]);
+	if (dq.size() > n - t)
+		while (dq.size() > n - t)
+			dq.pop_back();
+	for (auto i : dq)
+		cout << char(i);
+	cout << '\n';
+}
