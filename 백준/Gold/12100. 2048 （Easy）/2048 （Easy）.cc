b77a51a1b89e0d0d0577b85a1a0ab41635f6b3b8
최연우 <50083524+wafla@users.noreply.github.com>
2023-05-18 22:46:00 +0900

    [Gold II] Title: 2048 (Easy), Time: 4 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
@@ -6,45 +6,57 @@
 #include <deque>
 #include <math.h>
 #include <memory.h>
-#define x first
-#define y second
+#include <stack>
+#include <map>
+#include <set>
+#include <bitset>
+#define X first
+#define Y second
+#define INF 0x3f3f3f3f
+#define MOD 1000000007
+#define ll long long
+#define pi pair<int, int>
 using namespace std;
-int n, maxx = 0;
-int find_max(int arr[][21])
+int board[21][21] = { 0 }, value[11] = { 0 };
+int n, ans = 0;
+int findMax()
 {
 	int x = 0;
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
-			if (arr[i][j] > x)
-				x = arr[i][j];
+			if (board[i][j] > x)
+				x = board[i][j];
 	return x;
 }
-void move(int arr[][21], int dir, int cnt)
+bool compare(int(*board)[21], int(*tmp)[21])
 {
-	if (cnt == 6)
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			if (board[i][j] != tmp[i][j])
+				return 1;
+	return 0;
+}
+void copy(int (*board)[21], int (*tmp)[21])
+{
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			tmp[i][j] = board[i][j];
+}
+void move(int dir)
+{
+	if (dir == 0)
 	{
-		int x = find_max(arr);
-		if (x > maxx)
-			maxx = x;
-		return;
-	}
-	if (dir == 1)//위
-	{
-		int tmp[21][21] = { 0 };
-		for (int i = 0; i < n; i++)
-			for (int j = 0; j < n; j++)
-				tmp[i][j] = arr[i][j];
 		for (int j = 0; j < n; j++)//위로 올리기
 		{
 			for (int i = 1; i < n; i++)
 			{
-				if (tmp[i][j] != 0 && tmp[i - 1][j] == 0)
+				if (board[i][j] != 0 && board[i - 1][j] == 0)
 				{
 					int k = i;
-					while (tmp[k][j] != 0 && tmp[k - 1][j] == 0 && k != 0)
+					while (board[k][j] != 0 && board[k - 1][j] == 0 && k != 0)
 					{
-						tmp[k - 1][j] = tmp[k][j];
-						tmp[k][j] = 0;
+						board[k - 1][j] = board[k][j];
+						board[k][j] = 0;
 						k--;
 					}
 				}
@@ -54,10 +66,10 @@ void move(int arr[][21], int dir, int cnt)
 		{
 			for (int i = 0; i < n - 1; i++)
 			{
-				if (tmp[i][j] == tmp[i + 1][j])
+				if (board[i][j] == board[i + 1][j])
 				{
-					tmp[i][j] = 2 * tmp[i][j];
-					tmp[i + 1][j] = 0;
+					board[i][j] = 2 * board[i][j];
+					board[i + 1][j] = 0;
 					i++;
 				}
 			}
@@ -66,40 +78,32 @@ void move(int arr[][21], int dir, int cnt)
 		{
 			for (int i = 1; i < n; i++)
 			{
-				if (tmp[i][j] != 0 && tmp[i - 1][j] == 0)
+				if (board[i][j] != 0 && board[i - 1][j] == 0)
 				{
 					int k = i;
-					while (tmp[k][j] != 0 && tmp[k - 1][j] == 0 && k != 0)
+					while (board[k][j] != 0 && board[k - 1][j] == 0 && k != 0)
 					{
-						tmp[k - 1][j] = tmp[k][j];
-						tmp[k][j] = 0;
+						board[k - 1][j] = board[k][j];
+						board[k][j] = 0;
 						k--;
 					}
 				}
 			}
 		}
-		move(tmp, 1, cnt + 1);
-		move(tmp, 2, cnt + 1);
-		move(tmp, 3, cnt + 1);
-		move(tmp, 4, cnt + 1);
 	}
-	else if (dir == 2)//아래
+	else if (dir == 1)
 	{
-		int tmp[21][21] = { 0 };
-		for (int i = 0; i < n; i++)
-			for (int j = 0; j < n; j++)
-				tmp[i][j] = arr[i][j];
 		for (int j = 0; j < n; j++)//밑으로 내리기
 		{
 			for (int i = n - 2; i >= 0; i--)
 			{
-				if (tmp[i][j] != 0 && tmp[i + 1][j] == 0)
+				if (board[i][j] != 0 && board[i + 1][j] == 0)
 				{
 					int k = i;
-					while (tmp[k][j] != 0 && tmp[k + 1][j] == 0 && k != n - 1)
+					while (board[k][j] != 0 && board[k + 1][j] == 0 && k != n - 1)
 					{
-						tmp[k + 1][j] = tmp[k][j];
-						tmp[k][j] = 0;
+						board[k + 1][j] = board[k][j];
+						board[k][j] = 0;
 						k++;
 					}
 				}
@@ -109,10 +113,10 @@ void move(int arr[][21], int dir, int cnt)
 		{
 			for (int i = n - 1; i > 0; i--)
 			{
-				if (tmp[i][j] == tmp[i - 1][j])
+				if (board[i][j] == board[i - 1][j])
 				{
-					tmp[i][j] = 2 * tmp[i][j];
-					tmp[i - 1][j] = 0;
+					board[i][j] = 2 * board[i][j];
+					board[i - 1][j] = 0;
 					i--;
 				}
 			}
@@ -121,40 +125,32 @@ void move(int arr[][21], int dir, int cnt)
 		{
 			for (int i = n - 2; i >= 0; i--)
 			{
-				if (tmp[i][j] != 0 && tmp[i + 1][j] == 0)
+				if (board[i][j] != 0 && board[i + 1][j] == 0)
 				{
 					int k = i;
-					while (tmp[k][j] != 0 && tmp[k + 1][j] == 0 && k != n - 1)
+					while (board[k][j] != 0 && board[k + 1][j] == 0 && k != n - 1)
 					{
-						tmp[k + 1][j] = tmp[k][j];
-						tmp[k][j] = 0;
+						board[k + 1][j] = board[k][j];
+						board[k][j] = 0;
 						k++;
 					}
 				}
 			}
 		}
-		move(tmp, 1, cnt + 1);
-		move(tmp, 2, cnt + 1);
-		move(tmp, 3, cnt + 1);
-		move(tmp, 4, cnt + 1);
 	}
-	else if (dir == 3)//오른쪽
+	else if (dir == 2)
 	{
-		int tmp[21][21] = { 0 };
-		for (int i = 0; i < n; i++)
-			for (int j = 0; j < n; j++)
-				tmp[i][j] = arr[i][j];
 		for (int i = 0; i < n; i++)//오른쪽으로 밀기
 		{
 			for (int j = n - 2; j >= 0; j--)
 			{
-				if (tmp[i][j] != 0 && tmp[i][j + 1] == 0)
+				if (board[i][j] != 0 && board[i][j + 1] == 0)
 				{
 					int k = j;
-					while (tmp[i][k] != 0 && tmp[i][k + 1] == 0 && k != n - 1)
+					while (board[i][k] != 0 && board[i][k + 1] == 0 && k != n - 1)
 					{
-						tmp[i][k + 1] = tmp[i][k];
-						tmp[i][k] = 0;
+						board[i][k + 1] = board[i][k];
+						board[i][k] = 0;
 						k++;
 					}
 				}
@@ -164,10 +160,10 @@ void move(int arr[][21], int dir, int cnt)
 		{
 			for (int j = n - 1; j > 0; j--)
 			{
-				if (tmp[i][j] == tmp[i][j - 1])
+				if (board[i][j] == board[i][j - 1])
 				{
-					tmp[i][j] = 2 * tmp[i][j];
-					tmp[i][j - 1] = 0;
+					board[i][j] = 2 * board[i][j];
+					board[i][j - 1] = 0;
 					j--;
 				}
 			}
@@ -176,40 +172,32 @@ void move(int arr[][21], int dir, int cnt)
 		{
 			for (int j = n - 2; j >= 0; j--)
 			{
-				if (tmp[i][j] != 0 && tmp[i][j + 1] == 0)
+				if (board[i][j] != 0 && board[i][j + 1] == 0)
 				{
 					int k = j;
-					while (tmp[i][k] != 0 && tmp[i][k + 1] == 0 && k != n - 1)
+					while (board[i][k] != 0 && board[i][k + 1] == 0 && k != n - 1)
 					{
-						tmp[i][k + 1] = tmp[i][k];
-						tmp[i][k] = 0;
+						board[i][k + 1] = board[i][k];
+						board[i][k] = 0;
 						k++;
 					}
 				}
 			}
 		}
-		move(tmp, 1, cnt + 1);
-		move(tmp, 2, cnt + 1);
-		move(tmp, 3, cnt + 1);
-		move(tmp, 4, cnt + 1);
 	}
-	else if (dir == 4)//왼쪽
+	else if (dir == 3)
 	{
-		int tmp[21][21] = { 0 };
-		for (int i = 0; i < n; i++)
-			for (int j = 0; j < n; j++)
-				tmp[i][j] = arr[i][j];
 		for (int i = 0; i < n; i++)//왼쪽으로 밀기
 		{
 			for (int j = 1; j < n; j++)
 			{
-				if (tmp[i][j] != 0 && tmp[i][j - 1] == 0)
+				if (board[i][j] != 0 && board[i][j - 1] == 0)
 				{
 					int k = j;
-					while (tmp[i][k] != 0 && tmp[i][k - 1] == 0 && k != 0)
+					while (board[i][k] != 0 && board[i][k - 1] == 0 && k != 0)
 					{
-						tmp[i][k - 1] = tmp[i][k];
-						tmp[i][k] = 0;
+						board[i][k - 1] = board[i][k];
+						board[i][k] = 0;
 						k--;
 					}
 				}
@@ -219,10 +207,10 @@ void move(int arr[][21], int dir, int cnt)
 		{
 			for (int j = 0; j < n - 1; j++)
 			{
-				if (tmp[i][j] == tmp[i][j + 1])
+				if (board[i][j] == board[i][j + 1])
 				{
-					tmp[i][j] = 2 * tmp[i][j];
-					tmp[i][j + 1] = 0;
+					board[i][j] = 2 * board[i][j];
+					board[i][j + 1] = 0;
 					j++;
 				}
 			}
@@ -231,36 +219,60 @@ void move(int arr[][21], int dir, int cnt)
 		{
 			for (int j = 1; j < n; j++)
 			{
-				if (tmp[i][j] != 0 && tmp[i][j - 1] == 0)
+				if (board[i][j] != 0 && board[i][j - 1] == 0)
 				{
 					int k = j;
-					while (tmp[i][k] != 0 && tmp[i][k - 1] == 0 && k != 0)
+					while (board[i][k] != 0 && board[i][k - 1] == 0 && k != 0)
 					{
-						tmp[i][k - 1] = tmp[i][k];
-						tmp[i][k] = 0;
+						board[i][k - 1] = board[i][k];
+						board[i][k] = 0;
 						k--;
 					}
 				}
 			}
 		}
-		move(tmp, 1, cnt + 1);
-		move(tmp, 2, cnt + 1);
-		move(tmp, 3, cnt + 1);
-		move(tmp, 4, cnt + 1);
+	}
+}
+void solve(int cnt)
+{
+	int num = findMax();
+	if (num <= value[cnt])
+		return;
+	if (cnt == 5)
+	{
+		ans = max(ans, num);
+		int tmp = ans;
+		while (cnt > 0)
+		{
+			value[cnt--] = tmp;
+			tmp /= 2;
+		}
+		return;
+	}
+	int arr[21][21] = { 0 };
+	copy(board, arr);
+	for (int i = 0; i < 4; i++)
+	{
+		move(i);
+		if (compare(board, arr))
+		{
+			solve(cnt + 1);
+			copy(arr, board);
+		}
 	}
 }
 int main()
 {
-	ios::sync_with_stdio(NULL);
+	ios::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
 	cin >> n;
-	int arr[21][21] = { 0 };
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
-			cin >> arr[i][j];
-	move(arr, 1, 1);//위
-	move(arr, 2, 1);//아래
-	move(arr, 3, 1);//오른쪽
-	move(arr, 4, 1);//왼쪽
-	cout << maxx;
+		{
+			cin >> board[i][j];
+			if (ans < board[i][j])
+				ans = board[i][j];
+		}
+	solve(0);
+	cout << ans << '\n';
 }
--- 백준/Gold/12100. 2048 （Easy）/README.md
+++ 백준/Gold/12100. 2048 （Easy）/README.md
@@ -4,11 +4,11 @@
 
 ### 성능 요약
 
-메모리: 2024 KB, 시간: 8 ms
+메모리: 2028 KB, 시간: 4 ms
 
 ### 분류
 
-백트래킹(backtracking), 브루트포스 알고리즘(bruteforcing), 구현(implementation), 시뮬레이션(simulation)
+백트래킹, 브루트포스 알고리즘, 구현, 시뮬레이션
 
 ### 문제 설명
 
