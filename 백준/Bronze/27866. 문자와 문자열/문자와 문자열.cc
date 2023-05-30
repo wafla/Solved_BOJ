29caafc57e9b17cebd5a8a3346cbcf0997266c40
최연우 <50083524+wafla@users.noreply.github.com>
2023-05-30 22:10:15 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2104 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#include <bitset>
+#include <numeric>
+#define X first
+#define Y second
+#define INF 0x3f3f3f3f
+#define MOD 1000000007
+#define ll long long
+#define pi pair<int, int>
+using namespace std;
+int a[101][101] = { 0 }, b[101][101] = { 0 };
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	string s;
+	cin >> s;
+	int n;
+	cin >> n;
+	cout << s[n - 1] << '\n';
+}
