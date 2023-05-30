c0ad8b8c6c451de118abd84f69ad6f84addeea32
최연우 <50083524+wafla@users.noreply.github.com>
2023-05-30 22:11:10 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#include <bitset>
+#include <numeric>
+#define X first
+#define Y second
+#define INF 0x3f3f3f3f
+#define MOD 1000000007
+#define ll long long
+#define pi pair<int, int>
+using namespace std;
+int a[101][101] = { 0 }, b[101][101] = { 0 };
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	int a, b;
+	cin >> a >> b;
+	cout << a * b << '\n';
+}
