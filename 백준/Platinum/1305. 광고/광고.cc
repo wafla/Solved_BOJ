a9412e7bdc03d412212490034ac07330a20e1527
최연우 <50083524+wafla@users.noreply.github.com>
2023-06-26 18:15:14 +0900

    [Platinum IV] Title: 광고, Time: 12 ms, Memory: 7076 KB -BaekjoonHub

---
+++ 백준/Platinum/1305. 광고/광고.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#include <bitset>
+#define X first
+#define Y second
+#define INF 0x3f3f3f3f
+#define MOD 1000000007
+#define ll long long
+#define pi pair<int, int>
+using namespace std;
+int L;
+string s;
+int getPI()
+{
+	int j = 0, ret = 0;
+	vector<int> PI(L);
+	for (int i = 1; i < L; i++)
+	{
+		while (j > 0 && s[i] != s[j])
+			j = PI[j - 1];
+		if (s[i] == s[j])
+		{
+			PI[i] = ++j;
+			ret = max(PI[i], ret);
+		}
+	}
+	return PI[L-1];
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	cin >> L >> s;
+	int PI = getPI();
+	cout << L - PI << '\n';
+}
