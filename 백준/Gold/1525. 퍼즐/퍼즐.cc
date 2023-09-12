f93ffd688c4e92f49f07244fb639c677cff5accd
최연우 <50083524+wafla@users.noreply.github.com>
2023-09-12 14:48:35 +0900

    [Gold II] Title: 퍼즐, Time: 436 ms, Memory: 29120 KB -BaekjoonHub

---
+++ 백준/Gold/1525. 퍼즐/퍼즐.cc
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <cmath>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#include <numeric>
+#define X first
+#define Y second
+#define INF 0x3f3f3f3f
+#define MOD 1000000007
+#define ll long long
+#define int long long
+#define pi pair<int,int>
+#define T pair<int, pair<vector<int>, int>>
+using namespace std;
+set<vector<int>> S;
+int check(vector<int> v)
+{
+	int flag = 0;
+	flag = S.count(v) ? 1 : 0;
+	S.insert(v);
+	return flag;
+}
+signed main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	vector<int> tmp(9), num = { 1,2,3,4,5,6,7,8,0 };
+	int idx = 0;
+	for (int i = 0; i < 9; i++)
+	{
+		cin >> tmp[i];
+		if (tmp[i] == 0)
+			idx = i;
+	}
+	check(tmp);
+	priority_queue<T,vector<T>,greater<T>> Q;
+	Q.push({ 0,{ tmp,idx } });
+	while (!Q.empty())
+	{
+		auto cur = Q.top();
+		Q.pop();
+		if (cur.Y.X == num)
+		{
+			cout << cur.X << '\n';
+			return 0;
+		}
+		int x = cur.Y.Y;
+		if (x % 3 == 0)
+		{
+			int x1 = x - 3, x2 = x + 1, x3 = x + 3;
+			if (x1 > -1)
+			{
+				vector<int> tmp_ = cur.Y.X;
+				int tmp_x = tmp_[x];
+				tmp_[x] = tmp_[x1];
+				tmp_[x1] = tmp_x;
+				if (!check(tmp_))
+					Q.push({ cur.X + 1,{tmp_,x1},});
+			}
+			if (x2 < 9)
+			{
+				vector<int> tmp_ = cur.Y.X;
+				int tmp_x = tmp_[x];
+				tmp_[x] = tmp_[x2];
+				tmp_[x2] = tmp_x;
+				if (!check(tmp_))
+					Q.push({ cur.X + 1,{tmp_,x2}, });
+			}
+			if (x3 < 9)
+			{
+				vector<int> tmp_ = cur.Y.X;
+				int tmp_x = tmp_[x];
+				tmp_[x] = tmp_[x3];
+				tmp_[x3] = tmp_x;
+				if (!check(tmp_))
+					Q.push({ cur.X + 1,{tmp_,x3}, });
+			}
+		}
+		else if (x % 3 == 1)
+		{
+			int x1 = x - 3, x2 = x - 1, x3 = x + 1, x4 = x + 3;
+			if (x1 > -1)
+			{
+				vector<int> tmp_ = cur.Y.X;
+				int tmp_x = tmp_[x];
+				tmp_[x] = tmp_[x1];
+				tmp_[x1] = tmp_x;
+				if (!check(tmp_))
+					Q.push({ cur.X + 1,{tmp_,x1}, });
+			}
+			if (x2 > -1)
+			{
+				vector<int> tmp_ = cur.Y.X;
+				int tmp_x = tmp_[x];
+				tmp_[x] = tmp_[x2];
+				tmp_[x2] = tmp_x;
+				if (!check(tmp_))
+					Q.push({ cur.X + 1,{tmp_,x2}, });
+			}
+			if (x3 < 9)
+			{
+				vector<int> tmp_ = cur.Y.X;
+				int tmp_x = tmp_[x];
+				tmp_[x] = tmp_[x3];
+				tmp_[x3] = tmp_x;
+				if (!check(tmp_))
+					Q.push({ cur.X + 1,{tmp_,x3}, });
+			}
+			if (x4 < 9)
+			{
+				vector<int> tmp_ = cur.Y.X;
+				int tmp_x = tmp_[x];
+				tmp_[x] = tmp_[x4];
+				tmp_[x4] = tmp_x;
+				if (!check(tmp_))
+					Q.push({ cur.X + 1,{tmp_,x4}, });
+			}
+		}
+		else if (x % 3 == 2)
+		{
+			int x1 = x - 3, x2 = x - 1, x3 = x + 3;
+			if (x1 > -1)
+			{
+				vector<int> tmp_ = cur.Y.X;
+				int tmp_x = tmp_[x];
+				tmp_[x] = tmp_[x1];
+				tmp_[x1] = tmp_x;
+				if (!check(tmp_))
+					Q.push({ cur.X + 1,{tmp_,x1}, });
+			}
+			if (x2 > -1)
+			{
+				vector<int> tmp_ = cur.Y.X;
+				int tmp_x = tmp_[x];
+				tmp_[x] = tmp_[x2];
+				tmp_[x2] = tmp_x;
+				if (!check(tmp_))
+					Q.push({ cur.X + 1,{tmp_,x2}, });
+			}
+			if (x3 < 9)
+			{
+				vector<int> tmp_ = cur.Y.X;
+				int tmp_x = tmp_[x];
+				tmp_[x] = tmp_[x3];
+				tmp_[x3] = tmp_x;
+				if (!check(tmp_))
+					Q.push({ cur.X + 1,{tmp_,x3}, });
+			}
+		}
+	}
+	cout << -1 << '\n';
+}
