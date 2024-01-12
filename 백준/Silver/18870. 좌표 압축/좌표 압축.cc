2b9f3217c99fbca94d370c2f98529001c9d08709
최연우 <50083524+wafla@users.noreply.github.com>
2024-01-12 23:42:35 +0900

    [Silver II] Title: 좌표 압축, Time: 468 ms, Memory: 9840 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+signed main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	int n;
+	cin >> n;
+	vector<int> v, v2;
+    v.resize(n);
+    v2.resize(n);
+	for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+        v2[i]=v[i];
+    }
+	sort(v.begin(), v.end());
+	v.resize(unique(v.begin(), v.end()) - v.begin());
+	for (auto i : v2)
+		cout << lower_bound(v.begin(),v.end(),i)-v.begin() << " ";
+	cout << '\n';
+    return 0;
+}
