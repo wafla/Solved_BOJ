9846ad039069e28811f1c5099cb85e1bbe00f522
최연우 <50083524+wafla@users.noreply.github.com>
2024-08-02 23:49:30 +0900

    [Silver IV] Title: 정수 제곱근, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <tuple>
+#define X first
+#define Y second
+#define INF 0x3f3f3f3f
+#define LINF 9223372036854775807
+#define MOD 1000000000
+#define ll long long
+#define int long long
+#define pi pair<int, int>
+using namespace std;
+signed main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	int n;
+	cin >> n;
+	int x = sqrt(n);
+	if (x * x >= n)
+		cout << x << '\n';
+	else
+		cout << x + 1 << '\n';
+}
