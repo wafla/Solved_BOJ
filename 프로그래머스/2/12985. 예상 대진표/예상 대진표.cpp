b839d71bb9d209b3fca4f20e2190447c8d86c127
최연우 <jjangguzi@naver.com>
2024-10-28 13:29:15 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -1,23 +1,15 @@
 #include <iostream>
-
+#include <math.h>
 using namespace std;
 
 int solution(int n, int a, int b)
 {
     int answer = 0;
     
-    while(1)
+    while(a!=b)
     {
-        if(a==b)
-            break;
-        if(a%2)
-            a=(a+1)/2;
-        else
-            a/=2;
-        if(b%2)
-            b=(b+1)/2;
-        else
-            b/=2;
+        a=ceil(double(a)/2);
+        b=ceil(double(b)/2);
         answer++;
     }
 
