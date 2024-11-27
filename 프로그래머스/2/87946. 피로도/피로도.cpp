ae9f53377a9d7c1e1ded3c39c1f3ad57809fbf3c
최연우 <jjangguzi@naver.com>
2024-11-28 01:24:01 +0900

    [level 2] Title: 피로도, Time: 0.89 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+using namespace std;
+bool visited[9]={0};
+vector<vector<int>> v;
+int ans = 0;
+void dfs(int k, int idx, int cnt)
+{
+    if(k<0)
+        return;
+    ans = max(ans, cnt);
+    for(int i=0;i<v.size();i++)
+    {
+        int a = v[i][0];
+        int b = v[i][1];
+        if(k >= a && !visited[i])
+        {
+            visited[i]=true;
+            dfs(k-b,i,cnt+1);
+            visited[i]=false;
+        }
+    }
+}
+int solution(int k, vector<vector<int>> dungeons) {
+    v = dungeons;
+    for(int i=0;i<dungeons.size();i++)
+    {
+        int a = dungeons[i][0];
+        int b = dungeons[i][1];
+        if(k >= a)
+        {
+            visited[i]=true;
+            dfs(k-b,i,1);
+            visited[i]=false;
+        }
+    }
+    return ans;
+}
